bd5e9ac587604a2d660b91226129aa0b3adcb363
joeLepper <josephlepper@gmail.com>
2013-08-08 22:22:24 -0700

    add callback

--- flasher.js
+++ flasher.js
@@ -1,16 +1,16 @@
 var gpio = require('pi-gpio')
 
-  , flash = function(interval){
+  , flash = function(interval,callback){
 
       gpio.open(22,function(){
         gpio.write(22,1,function(){
           setTimeout(function(){
             gpio.write(22,0,function(){
-              gpio.close(22)
+              gpio.close(22,callback())
             })
           },interval)
         })
       })
     }
 
-flash(500)
+exports.flash = flash
